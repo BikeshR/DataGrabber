ed574e1a98a90c650b0f7f88e1ceedd5a429f227
Bikesh Rana <bksh.rana@hotmail.com>
2019-05-23 23:49:18 +0100

    Add project files.

---
+++ DataGrabber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataGrabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
