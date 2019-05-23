a0ea3cbc2729af163dc512bb03755364acb67f10
Bikesh Rana <bksh.rana@hotmail.com>
2019-05-24 00:00:07 +0100

    Test API GET request to get the JSON file.

--- DataGrabber/Program.cs
+++ DataGrabber/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace DataGrabber
 {
-    class Program
+    public static class ApiCall
+    {
+        public static async Task GetJson()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo");
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task.Run(async () =>
+            {
+                await ApiCall.GetJson();
+            }).GetAwaiter().GetResult();
+
+
         }
     }
 }
