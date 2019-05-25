c4c0938a512f24b663ed7824ee69020664a50ebe
Bikesh Rana <bksh.rana@hotmail.com>
2019-05-25 14:43:54 +0100

    Updated model for Daily Price.

---
+++ DataGrabber.Models/DailyPrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataGrabber.Models
+{
+    public class DailyPrice
+    {
+        public string Symbol { get; set; }
+        public DateTime PriceDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public Decimal Open { get; set; }
+        public Decimal High { get; set; }
+        public Decimal Low { get; set; }
+        public Decimal Close { get; set; }
+        public Decimal AdjClose { get; set; }
+        public int Volume { get; set; }
+        public Decimal DividendAmount { get; set; }
+        public Decimal SplitCoefficient { get; set;
+}
